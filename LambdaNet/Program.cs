57905b6715f1ad1663e0e92d8cde88cd46e2c90e
Dragoni222 <joelharr222@gmail.com>
2023-08-07 19:48:25 -0700

    Single Layer Relative Network
    Signed-off-by: Dragoni222 <joelharr222@gmail.com>

--- LambdaNet/Program.cs
+++ LambdaNet/Program.cs
@@ -13,6 +13,29 @@
 
 
 using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Complex32;
+using Vector = MathNet.Numerics.LinearAlgebra.Double.Vector;
+
+
+Node[] network = new Node[3];
+for (int i = 0; i < 3; i++)
+{
+    network[i] = new Node(3);
+}
+
+Random random = new Random();
+for (int i = 0; i < 10000; i++)
+{
+    int startValue = random.Next(0,10);
+    double[] expected = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    expected[startValue] = 1;
+    double[] outputs = network[0].ChangeConfidencesTrain(new double[] { startValue, 2, 3}, expected, 1);
+    Console.WriteLine("\n\n Expected: "+ outputs[startValue] +" Full:" + string.Join(", ", outputs));
+}
+
+
+    
+
 class Node
 {
     private Matrix<double> Weights;  //one weight vector for each number 
@@ -20,18 +43,39 @@ class Node
 
     public Node(int totalNodes)
     {
-        Weights = Matrix<double>.Build.Dense(totalNodes, 10); //Maybe reversed?
-        Biases = Vector<double>.Build.Dense(totalNodes);
+        Weights = Matrix<double>.Build.Random(10, totalNodes);
+        Biases = Vector<double>.Build.Random(10);
 
     }
 
-    public double[] ChangeConfidences(double[] currentValues)
+    public double[] ChangeConfidences(double[] inputValues)
     {
-        Vector<double> values = Vector<double>.Build.DenseOfArray(currentValues); // totalNodes length start vector
+        Vector<double> values = Vector<double>.Build.DenseOfArray(inputValues); // totalNodes length start vector
 
         Vector<double> resultant = values * Weights;
         Vector<double> withBiases = resultant + Biases;
         return withBiases.Map(result => Sigmoid(result)).ToArray();
+    }
+
+    public double[] ChangeConfidencesTrain(double[] inputValues, double[] expectedValues, double trainSpeed)
+    {
+        Vector<double> values = Vector<double>.Build.DenseOfArray(inputValues); // Input vector
+        Vector<double> resultant = Weights * values; // Weighted sum
+        Vector<double> zValues = resultant + Biases; // Adding biases
+        Vector<double> final = zValues.Map(result => Sigmoid(result)); // Applying activation function
+        for (int i = 0; i < Biases.Count; i++)
+        {
+            double loss = final[i] - expectedValues[i];
+            double sigmoidDerivative = final[i] * (1 - final[i]);
+            Biases[i] -= loss * sigmoidDerivative * trainSpeed;
+            for (int j = 0; j < Weights.ColumnCount; j++)
+            {
+                Weights[i, j] -= loss * values[j] * sigmoidDerivative * trainSpeed;
+            }
+            
+        }
+
+        return final.ToArray();
     }
     
     static double Sigmoid(double x)
@@ -45,3 +89,4 @@ class Node
     }
 
 }
+
